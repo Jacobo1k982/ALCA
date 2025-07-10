97712efb7beec4568c8c8f8fc2db1acd149f21ec
Jacobo1k982 <askmecr@gmail.com>
2025-07-10 09:24:05 -0600

    v2

--- src/Components/QuienesSomos.jsx
+++ src/Components/QuienesSomos.jsx
@@ -6,17 +6,17 @@ const QuienesSomos = () => {
             <div className="max-w-7xl mx-auto">
                 {/* Encabezado */}
                 <div className="text-center mb-12">
-                    <h2 className="text-3xl sm:text-4xl font-bold text-amber-900 mb-4">ALCA TEXTIL</h2>
-                    <h3 className="text-2xl sm:text-3xl font-semibold text-gray-800">SOBRE NOSOTROS</h3>
+                    <h2 className="text-3xl sm:text-4xl font-bold text-green-600 mb-4">BORDADOS ALCA</h2>
+                    <h3 className="text-2xs sm:text-3xl font-semibold text-green-800">SOBRE NOSOTROS</h3>
                 </div>
 
                 <div className="flex flex-col lg:flex-row gap-12 items-center">
                     {/* Sección de texto */}
                     <div className="lg:w-1/2">
                         <p className="text-gray-700 text-lg leading-relaxed">
-                            Somos una empresa con más de 10 años de experiencia en el mundo textil, desde hace 6 años con unas instalaciones propias, donde se realiza todo el proceso partiendo de la materia prima hasta el empaquetado final e incluso con la posibilidad de envío a cliente final, trabajando con drop-shipping.
+                            Somos una empresa de confecciones textil conformada por un equipo de trabajo con alto grado de profesionalismo y competitividad, dedicados a ofrecer la producción de prendas de vestir y complementos a pedido que demanda el mercado textil, basado en técnicas innovadoras, contando con maquinaria moderna y especializada en costura, contribuyendo a la mejora continua en la elaboración y control de cada proceso garantizando la calidad del producto para satisfacción de nuestros clientes.
                             <br /><br />
-                            Las instalaciones principales se encuentran en las proximidades de Zaragoza, donde contamos con máquinas de corte automático, corte manual, impresoras, calandra, máquinas de confección y por supuesto una zona de repasado, almacenaje y envío. Nos encargamos de todo el proceso.
+                            Las instalaciones principales se encuentran en San José, Pavas centro, donde contamos con máquinas de corte automático, corte manual, impresoras, máquinas de confección y por supuesto una zona de repasado, almacenaje y envío. Nos encargamos de todo el proceso.
                         </p>
                     </div>
 
